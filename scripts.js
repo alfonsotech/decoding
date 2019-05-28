f8ed651738488c1b66d078b94f4319b88b4ca09a
alfonsotech <alfonsotech@nym.hush.com>
2019-05-27 21:14:32 -0400

    First Commit

---
+++ scripts.js
@@ -0,0 +1,37 @@
+$(function() {
+    console.log( "ready!" );
+    fetch('/data.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    myJson.forEach(function(element) {
+      let unit;
+      if(element.type === 'capstone') {
+        unit = `
+        <div class="card capstone" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text">${element.description}</p>
+        <p class="card-text tech-stack">${element.tech}</p>
+        <a href="${element.repo}" target="_blank" class="btn btn-primary">Learn More</a>
+        </div>
+        </div>
+        `
+      } else {
+        unit = `
+        <div class="card project" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text">${element.description}</p>
+        <p class="card-text tech-stack">${element.tech}</p>
+        <a href=${element.repo} target="_blank" class="btn btn-primary">Learn More</a>
+        </div>
+        </div>
+        `
+      }
+    $('#curriculum').append(unit)
+    });
+
+  });
+});
